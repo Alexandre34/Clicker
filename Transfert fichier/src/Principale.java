08e798e0d5d1ffc1f72319aefa2be8513575135b
Alexandre <Alexandre@DESKTOP-RL2FSHJ>
2017-01-23 19:06:57 +0100

    Ceci est un essai.

--- Transfert fichier/src/Principale.java
+++ Transfert fichier/src/Principale.java
@@ -2,7 +2,7 @@
 public class Principale {
 
 	public static void main(String[] args) {
-		System.out.println("Ceci est un transfert de fichier.");
+		System.out.println("Ceci est un transfert.");
 		
 	}
 
